4c1b2cf5ab8a440ce344ff4c547308f4a5db73e4
Neema <patelneema93@gmail.com>
2022-02-18 09:07:14 -0500

    java basic

---
+++ variable.java
@@ -0,0 +1,16 @@
+public class variable{
+    public static void main(String[] args){
+        int myNum = 15;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool = true;
+String myText = "Hello";
+        System.out.println(myNum); 
+        String myStr = "Neema";
+        System.out.println(myStr);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+}
